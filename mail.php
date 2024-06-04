f8f513e913f26387fab38154f0713beed9344ca2
Melkid69 <167970274+Melkid45@users.noreply.github.com>
2024-06-04 18:09:26 +0600

    php

--- mail.php
+++ mail.php
@@ -1,11 +1,10 @@
 <?php
-$to = "melkidwork@gmail.com"; // емайл получателя данных из формы
-$tema = "Отправка данных"; // тема полученного емайла
-$message = "Ваше имя: ".$_POST['name']."<br>";//присвоить переменной значение, полученное из формы name=name
-$message .= "E-mail: ".$_POST['email']."<br>"; //полученное из формы name=email
-$message .= "Номер телефона: ".$_POST['phone']."<br>"; //полученное из формы name=phone
-$message .= "Сообщение: ".$_POST['message']."<br>"; //полученное из формы name=message
-$headers = 'MIME-Version: 1.0' . "\r\n"; // заголовок соответствует формату плюс символ перевода строки
-$headers .= 'Content-type: text/html; charset=utf-8' . "\r\n"; // указывает на тип посылаемого контента
-mail($to, $tema, $message, $headers); //отправляет получателю на емайл значения переменных
+$to = "melkidwork@gmail.com";
+$tema = "Отправка данных";
+$message = "Ваше имя: ".$_POST['name']."<br>";
+$message .= "Вопрос: ".$_POST['que']."<br>";
+$message .= "Номер телефона: ".$_POST['tel']."<br>";
+$headers = 'MIME-Version: 1.0' . "\r\n";
+$headers .= 'Content-type: text/html; charset=utf-8' . "\r\n";
+mail($to, $tema, $message, $headers);
 ?>
